cfe371f3f6e13c4b504dea49bf522abd57b11a6e
Shunmugam V <shunmugamv@Shunmugams-MacBook-Pro-2.local>
2022-09-22 10:03:26 +0530

    wg add example without using wait group

--- sync-pkg/wait-group/main.go
+++ sync-pkg/wait-group/main.go
@@ -13,13 +13,25 @@ func sleepRoutine(m data, w *sync.WaitGroup) {
 	fmt.Printf("good morning: %s\n", m.message)
 }
 
+func sleepRoutine1(m data) {
+	fmt.Printf("good night: %s\n", m.message)
+	time.Sleep(m.sleepTime)
+	fmt.Printf("good morning: %s\n", m.message)
+}
+
 type data struct {
 	message   string
 	sleepTime time.Duration
 }
 
 func main() {
+	withoutWaitGroup()
+	fmt.Println("++++++++++++++++++++++++++++++")
+	withWaitGroup()
 
+}
+
+func withWaitGroup() {
 	var wg sync.WaitGroup
 
 	for _, d := range []data{
@@ -37,6 +49,24 @@ func main() {
 	}
 
 	wg.Wait()
+
+	fmt.Println("all go routines completed")
+}
+
+func withoutWaitGroup() {
+
+	for _, d := range []data{
+		{
+			message:   "go-1-without-wg",
+			sleepTime: 5 * time.Second,
+		},
+		{
+			message:   "go-2-without-wg",
+			sleepTime: 10 * time.Second,
+		},
+	} {
+		go sleepRoutine1(d)
+	}
 
 	fmt.Println("all go routines completed")
 }
